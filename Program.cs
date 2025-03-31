eb94e9b07b70e43f95e52abbede32023d76ea2ff
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-03-31 08:43:58 -0300

    Programa que converte um valor em milhas para valor em km

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double em_milhas;
+double em_km;
+
+Console.Write("Entre com a medida (em milhas): ");
+em_milhas = Convert.ToDouble(Console.ReadLine()!);
+em_km = em_milhas * 1.609;
+Console.Clear();
+
+Console.WriteLine($"Medida em milhas: {em_milhas}");
+Console.WriteLine($"Medida em km: {em_km}");
